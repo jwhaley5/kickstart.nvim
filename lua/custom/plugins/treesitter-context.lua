fda67d9d162d5c4ab49d1ee8700f99af99a806a7
jwhaley5 <johnWwhaley@gmail.com>
2026-05-10 21:40:36 -0400

    upstream updates

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -1,13 +1,13 @@
-return {
-  'nvim-treesitter/nvim-treesitter-context',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {
-    enable = true,
-    max_lines = 3, -- Set 0 to disable the limit.
-    min_window_height = 0,
-    line_numbers = true,
-    multiline_threshold = 20,
-    trim_scope = 'outer',
-    mode = 'cursor',
-  },
+vim.pack.add {
+  'https://github.com/nvim-treesitter/nvim-treesitter-context',
+}
+
+require('treesitter-context').setup {
+  enable = true,
+  max_lines = 3, -- Set 0 to disable the limit.
+  min_window_height = 0,
+  line_numbers = true,
+  multiline_threshold = 20,
+  trim_scope = 'outer',
+  mode = 'cursor',
 }
